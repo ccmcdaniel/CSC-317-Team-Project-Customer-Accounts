49013e32663ff87350384a6cc3706ed72f6c87f2
Chad McDaniel <christopher.c.mcdaniel@usm.edu>
2023-02-27 11:19:25 -0600

    Add project files.

---
+++ CSC 317 Team Project Customer Accounts/Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC_317_Team_Project_Customer_Accounts
+{
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public decimal AccountBalance { get; set; }
+        public DateTime LastPaymentDate { get; set; }
+
+        public override string ToString()
+        {
+            //output the customer as a string.
+            return base.ToString();
+        }
+
+    }
+
+    class CustomerList
+    { 
+        private List<Customer> customerList;
+
+        void FindCustomer()
+        {
+            //blank
+        }
+
+        void AddCustomer()
+        {
+
+        }
+    
+        void RemoveCusomter()
+        {
+
+        }
+        
+        void EditCustomer()
+        {
+
+        }
+    }
+
+}
---
+++ CSC 317 Team Project Customer Accounts/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC_317_Team_Project_Customer_Accounts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
